1385597d83e76d6caa51dd75226282b352b3449a
Luca Isidoro <apprendista44@gmail.com>
2020-09-14 16:54:46 +0200

    parte di esercizio

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>Benvenuto nella lista post</h2>
+
+        <ul>
+          @foreach ($posts as $post)
+            <li>{{$post->user->name}} - {{$post->title}}</li>
+          @endforeach
+        </ul>
+      </div>
+    </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+->namespace('Admin')
+->middleware('auth')
+->group(function(){
+  Route::resource('posts','PostController');
+});
